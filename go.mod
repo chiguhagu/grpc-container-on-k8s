f848d8217497c06dc569186e7ee1b4a72650f172
chiguhagu <info@chiguhagu.com>
2021-07-01 18:34:08 +0900

    feat: add gRPC server (Only HealthCheck)

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/chiguhagu/grpc-container-on-k8s
 go 1.16
 
 require (
+	github.com/kisielk/gotool v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	google.golang.org/grpc v1.38.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,6 +11,10 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -32,6 +37,20 @@ func main() {
 		return s.ListenAndServe()
 	})
 
+	g := grpc.NewServer()
+	fmt.Println("start gRPC server")
+	wg.Go(func() error {
+		l, err := net.Listen("tcp", ":50051")
+		if err != nil {
+			fmt.Println("failed to listen port:", err)
+			return err
+		}
+		grpc_health_v1.RegisterHealthServer(g, health.NewServer())
+		reflection.Register(g)
+		err = g.Serve(l)
+		return err
+	})
+
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGTERM, os.Interrupt)
 	select {
@@ -41,6 +60,10 @@ func main() {
 	}
 
 	cancel()
+
+	fmt.Println("start to shutdown gRPC server gracefully")
+	g.GracefulStop()
+	fmt.Println("finish to shutdown gRPC server gracefully")
 
 	fmt.Println("start to shutdown OpenMetrics server gracefully")
 	if err := s.Shutdown(context.TODO()); err != nil {
